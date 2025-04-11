7a7261d81ef9ea1a5ec48367f6cfa000d8be8c81
ljbeal <louis.j.beal@gmail.com>
2025-04-11 17:24:59 +0200

    Migrate the Transport suite from remotemanager

---
+++ alchemy_test/transport/__init__.py
@@ -0,0 +1,5 @@
+from alchemy_test.transport.cp import cp
+from alchemy_test.transport.rsync import rsync
+from alchemy_test.transport.scp import scp
+
+__all__ = ["cp", "rsync", "scp"]
---
+++ alchemy_test/transport/cp.py
@@ -0,0 +1,22 @@
+"""
+Handles file transfer via `cp`
+"""
+
+import logging
+
+from alchemy_test.transport.transport import Transport
+
+logger = logging.getLogger(__name__)
+
+
+class cp(Transport):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        logger.info("created new cp transport")
+
+    def cmd(self, primary, secondary):
+        cmd = "mkdir -p {secondary} ; cp -r --preserve {primary} {secondary}"
+        base = cmd.format(primary=primary, secondary=secondary)
+        logger.debug(f'returning formatted cmd: "{base}"')
+        return base
