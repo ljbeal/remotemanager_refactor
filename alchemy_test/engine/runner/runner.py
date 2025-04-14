836085e49b3d6f2e9d192a2ccd606eb4a39795fa
Louis Beal <louis.j.beal@gmail.com>
2025-04-14 15:20:58 +0200

    Runner now creates all necessary files

--- alchemy_test/engine/runner/runner.py
+++ alchemy_test/engine/runner/runner.py
@@ -34,6 +34,8 @@ class Runner(UUIDMixin, ExecArgsMixin):
         self._files.add_file(self.local_dir, self.remote_dir, "jobscript", f"{self.name}-jobscript.sh")
         self._files.add_file(self.local_dir, self.remote_dir, "runfile", f"{self.name}-runfile.py")
 
+        self._files.add_file(self.local_dir, self.remote_dir, "resultfile", f"{self.name}-result.json")
+
     def __repr__(self) -> str:
         return self.name
 
@@ -89,7 +91,8 @@ class Runner(UUIDMixin, ExecArgsMixin):
             os.makedirs(self.local_dir)
         
         # generate and add the per-runner lines
-        self.parent.files.master.write(self.runline)
+        self.parent.files.master.write("export sourcedir=$PWD")
+        self.parent.files.master.append(self.runline)
         self.files.jobscript.write(f"{self.url.python} {self.files.runfile.name}")
 
         # write out the repository
@@ -99,7 +102,38 @@ class Runner(UUIDMixin, ExecArgsMixin):
         self.parent.files.repo.append(self.parent.function.raw_source)
 
         # write the actual run file
-        self.files.runfile.write("run file")
+
+        # script proper
+        runscript = [
+            "import importlib.util, os, sys, time",
+            "remote_path = os.path.expandvars('$sourcedir')",
+            f"path = os.path.join(remote_path, '{self.parent.files.repo.name}')",
+            "spec = importlib.util.spec_from_file_location('repo', path)",
+            "repo = importlib.util.module_from_spec(spec)",
+            "spec.loader.exec_module(repo)\n",
+            f"manifest = repo.Manifest(instance_uuid='{self.short_uuid}', filename='{self.parent.files.manifest.name}')",
+            "manifest.runner_mode = True",
+            # need to add this instance of the manifest for the function
+            "repo.manifest = manifest",
+            "starttime = int(time.time())",
+            "manifest.write('started')",
+            "vmaj, vmin, vpat, *extra = sys.version_info",
+            "if vmaj < 3:",
+            "\tmanifest.write('failed - Python Version')",
+            '\traise RuntimeError(f"Python version {vmaj}.{vmin}.{vpat} < 3.x.x")',
+            f"call_args = {self.call_args}",  # direct python dict serialisation only right now
+            f"try:\n\tresult = repo.{self.parent.function.name}(**call_args)",
+            "except Exception:\n\tmanifest.write('failed')",
+            "\traise",
+            "else:",
+            f"\tlast_reported_starttime = manifest.last_time('started').get('"  # comma
+            f"{self.short_uuid}', -1)",
+            "\tif last_reported_starttime <= starttime: # no output for outdated run",
+            "\t\tmanifest.write('completed')",
+            f"\t\tmanifest.dump_json"  # no comma
+            f"(result, '{self.files.resultfile.name}')",
+        ]
+        self.files.runfile.write("\n".join(runscript))
 
     def transfer(self):
         """
