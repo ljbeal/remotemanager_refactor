3a735a7297edc8d6a29f2b8507c9c8fc94623518
Louis Beal <louis.j.beal@gmail.com>
2025-04-14 15:35:36 +0200

    FileHandler can now mark files to not be sent with a run

--- alchemy_test/engine/files/filehandler.py
+++ alchemy_test/engine/files/filehandler.py
@@ -5,14 +5,17 @@ from alchemy_test.storage.trackedfile import TrackedFile
 
 class FileHandler:
 
-    __slots__ = ["_files"]
+    __slots__ = ["_files", "_do_not_send"]
     
     def __init__(self):
         
         self._files = {}
+        self._do_not_send = []
 
-    def add_file(self, local_dir, remote_dir, endpoint, filename):
+    def add_file(self, local_dir, remote_dir, endpoint, filename, send: bool = True):
         self._files[endpoint] = TrackedFile(local_dir, remote_dir, filename)
+        if not send:
+            self._do_not_send.append(endpoint)
 
     def __getattribute__(self, name):
         if name != "_files" and name in self._files:
@@ -22,3 +25,7 @@ class FileHandler:
     @property
     def files(self) -> List[TrackedFile]:
         return list(self._files.values())
+    
+    @property
+    def files_to_send(self) -> List[TrackedFile]:
+        return [f for e, f in self._files.items() if e not in self._do_not_send]
