7186aaf95a57ea9170c970bd8441976cc61b1283
Louis Beal <louis.j.beal@gmail.com>
2025-04-14 14:36:08 +0200

    Import get_package_root from remotemanager

--- alchemy_test/__init__.py
+++ alchemy_test/__init__.py
@@ -1,6 +1,15 @@
+import os
 from alchemy_test.engine.process import Process
 
 
 __all__ = ["Process"]
 
 __version__ = "0.0.1"
+
+
+
+def get_package_root() -> str:
+    """returns the abspath to the package root directory"""
+    return os.path.normpath(
+        os.path.join(os.path.abspath(__file__), os.pardir, os.pardir)
+    )
