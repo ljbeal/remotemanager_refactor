3aab00cfb601c35a78fa71b8ef67b9b522c6816d
ljbeal <louis.j.beal@gmail.com>
2025-05-06 09:36:20 +0200

    Add type hints to ensure_dir

--- alchemy_test/utils/ensure_dir.py
+++ alchemy_test/utils/ensure_dir.py
@@ -1,7 +1,7 @@
 import os
 
 
-def ensure_dir(dir):
+def ensure_dir(dir: str) -> str:
     """
     Ensure that string path to `dir` is correctly formatted
     ONLY ensures that the folder name ends with a "/", does not produce an
