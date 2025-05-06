06f3ba44ce93067567cd0906ff249a9573cd1aff
ljbeal <louis.j.beal@gmail.com>
2025-05-06 13:50:24 +0200

    Fix type hinting

--- alchemy_test/utils/random_string.py
+++ alchemy_test/utils/random_string.py
@@ -1,10 +1,11 @@
 import random
+from typing import List
 
 
 def random_string(length: int = 16) -> str:
     """Creates a random string of hexadecimal characters of len `len`"""
     choices = list(range(9)) + ["a", "b", "c", "d", "e", "f"]
-    output = []
+    output: List[str] = []
     for _ in range(length):
         output.append(str(random.choice(choices)))
 
