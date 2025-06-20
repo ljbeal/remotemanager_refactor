79efbc35a44473ef6441639a5b7a09b8d6c77f88
Louis Beal <louis.j.beal@gmail.com>
2025-06-20 14:05:42 +0200

    Add more flex to the async test for CI runs

--- tests/execution/test_async.py
+++ tests/execution/test_async.py
@@ -16,7 +16,7 @@ def basic(a: int, t: float) -> int:
 class TestAsync(BaseTestClass):
 
     sleeptime = 0.5
-    nRunners = 5
+    nRunners = 10
 
     def test_async(self, setpoint: str, asynchronous: bool):
         if setpoint == "init":
@@ -39,7 +39,13 @@ class TestAsync(BaseTestClass):
 
         dt = time.perf_counter() - t0
 
+        # async run should take approximately sleeptime, since they should run in parallel
+        # in practice, that's not the case when running in the CI, but we can add some flexibility
+        # to the time constraint.
         if asynchronous:
-            assert dt < self.sleeptime + 1
+            flex_time = self.sleeptime + 2
+            assert dt < flex_time
+            # just to make sure the test remains valid if we update it blindly
+            assert flex_time < self.sleeptime * self.nRunners
         else:
             assert dt >= self.sleeptime * self.nRunners
